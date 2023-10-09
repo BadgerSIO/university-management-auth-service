26cf27f05e4ea60fd1f5e6d0e8760914decc54d3
Saad Ibne Omar <86186495+BadgerSIO@users.noreply.github.com>
2023-10-09 16:16:22 +0600

    user interface and model created

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
-import mongoose from "mongoose";
-import config from "./config";
-import app from "./app";
+import mongoose from 'mongoose'
+import config from './config'
+import app from './app'
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.databaseUrl as string);
-    console.log("Database connected");
+    await mongoose.connect(config.databaseUrl as string)
+    console.log('Database connected')
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
-    });
+      console.log(`App listening on port ${config.port}`)
+    })
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-bootstrap();
+bootstrap()
